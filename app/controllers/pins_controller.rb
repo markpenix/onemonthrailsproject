49e6cc9f707cb2acceef55b104d7374b3085b0e2
Mark Penix <mark.penix@yahoo.com>
2014-11-11 07:02:56 -0600

    fixed pins controller auth error

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,7 @@ class PinsController < ApplicationController
 
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user! except: [:show]
+  before_action :authenticate_user!, except: [:show]
   respond_to :html
 
   def index
